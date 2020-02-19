07276b241e9508b72f95abb534315116060420c1
DarioLopes <dariolopes4v@gmail.com>
2020-02-19 11:38:34 +0100

    voila ma branch

--- models/index.js
+++ models/index.js
@@ -1,17 +1,20 @@
+//hello world
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/vinApp', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/vinApp', {
+  useNewUrlParser: true
+});
 // mongoose.connect('mongodb+srv://fahust:skisoboy@cavavin-dexvv.mongodb.net/test?retryWrites=true&w=majority');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log('Database is connected');
 });
 
 module.exports = app => {
-    app.models = {
-        Vin: require("./Vin"),
-        VinCommunity: require("./VinCommunity"),
-        User: require("./User"),
-        Tags: require("./Tags"),
-    };
+  app.models = {
+    Vin: require("./Vin"),
+    VinCommunity: require("./VinCommunity"),
+    User: require("./User"),
+    Tags: require("./Tags"),
+  };
 };
